9c6479c48629be9bc44464839ca55d47c8e09716
Fahim227 <islamfahim227@gmail.com>
2022-03-07 00:16:12 +0600

    Appointment time Automated

--- VFS_Bot/main.py
+++ VFS_Bot/main.py
@@ -8,6 +8,7 @@ base_url = "https://row1.vfsglobal.com/GlobalAppointment/Account/RegisteredLogin
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
+import concurrent.futures
 import time
 import pandas as pd
 from selenium import webdriver
@@ -47,7 +48,21 @@ def test():
         print()
 
 def page_two(driver,data):
-    url = "https://row1.vfsglobal.com/GlobalAppointment/Home/Index"
+    password = str(data['longin Password '])
+    # str(data['Passport number'].values[1])
+    email = data['Mail']
+    str(data['phone number'])
+    email_field = driver.find_element_by_id("EmailId")
+    pass_field = driver.find_element_by_id("Password")
+    email_field.send_keys(email)
+    pass_field.send_keys(password)
+    captcha = driver.find_element_by_id("CaptchaImage")
+    print(captcha.get_attribute("src"))
+    time.sleep(10)
+    driver.find_element_by_class_name("submitbtn").click()
+    time.sleep(3)
+    # page_two(driver, data.loc[1, :])
+    # url = "https://row1.vfsglobal.com/GlobalAppointment/Home/Index"
     print(driver.find_element_by_class_name("leftpanel-links").find_elements_by_class_name("inactive-link")[3].text)
     driver.find_element_by_class_name("leftpanel-links").find_elements_by_class_name("inactive-link")[3].click()
     page_three(driver,data)
@@ -66,43 +81,56 @@ def page_three(driver,data):
     phone.send_keys(str(data['phone number']))
     driver.find_elements_by_class_name('submitbtn')[1].click()
     try:
-        element = WebDriverWait(driver, 10).until(
+        element = WebDriverWait(driver, 5).until(
             EC.presence_of_element_located((By.CLASS_NAME, "btn"))
         )
         element.click()
+        time.sleep(3)
+        driver.find_element_by_class_name('frm-button').find_element_by_class_name('submitbtn').click()
+        time.sleep(3)
+        driver.find_element_by_id('btnContinueService').click()
+        time.sleep(3)
+        month_year = driver.find_element_by_class_name('fc-header-title').text
+        month = month_year.split(' ')[0]
+        print(month)
+
+        if month == data['targeted_month']:
+            print("select date")
+            selectDateAndTimeForAppointment(driver)
+        else:
+            return
     finally:
         print("Driver Quit")
     # end user data
     # time.sleep(3)
     # driver.find_element_by_class_name('btn').click()
-    time.sleep(3)
-    driver.find_element_by_class_name('frm-button').find_element_by_class_name('submitbtn').click()
-    time.sleep(3)
-    driver.find_element_by_id('btnContinueService').click()
-    time.sleep(3)
-    month_year = driver.find_element_by_class_name('fc-header-title').text
-    month = month_year.split(' ')[0]
-    print(month)
-
-# Press the green button in the gutter to run the script.
+def selectDateAndTimeForAppointment(driver):
+    calender = driver.find_element_by_class_name("fc-content")
+    weeks = driver.find_elements_by_class_name("fc-future")
+    for days in weeks:
+        days.click()
+        try:
+            # select time
+            timeRange = driver.find_element_by_id("TimeBandsDiv").find_elements_by_tag_name("tr")
+            timeRange[3].find_element_by_name("selectedTimeBand").click()
+            # confirm
+            driver.find_element_by_id("btnConfirm").click()
+            break
+        except:
+            continue
 if __name__ == '__main__':
     # Login
     driver = page_one()
     data = pd.read_csv('data.csv')
     # data['Reference number'].values[1]
-    password = str(data['longin Password '].values[1])
-    # str(data['Passport number'].values[1])
-    email = data['Mail'].values[1]
-    str(data['phone number'].values[1])
-    email_field = driver.find_element_by_id("EmailId")
-    pass_field = driver.find_element_by_id("Password")
-    email_field.send_keys(email)
-    pass_field.send_keys(password)
-    captcha = driver.find_element_by_id("CaptchaImage")
-    print(captcha.get_attribute("src"))
-    time.sleep(10)
-    driver.find_element_by_class_name("submitbtn").click()
-    time.sleep(3)
-    page_two(driver,data.loc[1,:])
+    d = []
+    for i in range(0,len(data)):
+        d.append((driver,data.loc[i,:]))
+        page_two(driver,data.loc[i,:])
+        driver = page_one()
+    # with concurrent.futures.ThreadPoolExecutor(max_workers=len(data)) as executor:
+    #     executor.map(lambda p: page_two(*p), d)
     test()
+
+
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
